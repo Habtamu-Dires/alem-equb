61d5575be15b62d7da47d77ee4b2bee462f29fb7
Habtamu Dires <habtadugna@gmail.com>
2025-03-20 14:56:07 +0300

    env-var-debug

--- alem-ekub-api/src/main/java/com/ekub/AlemEQubApplication.java
+++ alem-ekub-api/src/main/java/com/ekub/AlemEQubApplication.java
@@ -1,8 +1,5 @@
 package com.ekub;
 
-import com.ekub.config.AllowedOriginsConfig;
-import lombok.Getter;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -15,21 +12,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableConfigurationProperties
 @EnableAsync
-public class AlemEQubApplication implements CommandLineRunner {
-
-	private final AllowedOriginsConfig allowedOriginsConfig;
-
-	public AlemEQubApplication(AllowedOriginsConfig allowedOriginsConfig) {
-		this.allowedOriginsConfig = allowedOriginsConfig;
-	}
+public class AlemEQubApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlemEQubApplication.class, args);
-
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		allowedOriginsConfig.getOrigins().forEach(System.out::println);
-	}
 }
